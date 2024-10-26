0cf5c2a486daa1aae268142de303fc39e34a43e0
nitzano707 <nitzan.orot@gmail.com>
2024-10-26 23:37:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     // יצירת מופע של GROQ
     const groq = new GroqClient({
-        apiKey: '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'
+        apiKey: 'YOUR_GROQ_API_KEY'
     });
 
     // טיפול בגרירת קבצים
